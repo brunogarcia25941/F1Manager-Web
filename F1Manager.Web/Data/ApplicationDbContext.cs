8efc9473b3613aff1397fc0b985d3565568c908b
brunogarcia25941 <aluno25941@ipt.pt>
2026-05-25 15:48:14 +0100

    Configuracao ApplicationDbContext e string de ligação MySQL

---
+++ F1Manager.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using F1Manager.Web.Models;
+
+namespace F1Manager.Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        // Propriedades DbSet que representam as tabelas na base de dados
+        public DbSet<Campeonato> Campeonatos { get; set; }
+        public DbSet<Equipa> Equipas { get; set; }
+        public DbSet<Piloto> Pilotos { get; set; }
+        public DbSet<Corrida> Corridas { get; set; }
+        public DbSet<ResultadoCorrida> ResultadosCorridas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Como a tabela ResultadoCorrida é Muitos-para-Muitos, ela precisa de uma chave primária composta (PilotoId + CorridaId).
+            modelBuilder.Entity<ResultadoCorrida>()
+                .HasKey(rc => new { rc.PilotoId, rc.CorridaId });
+        }
+    }
+}
--- F1Manager.Web/Program.cs
+++ F1Manager.Web/Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using F1Manager.Web.Data; 
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Ligação à base de dados (MySQL)
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 // Add services to the container.
 builder.Services.AddRazorPages();
