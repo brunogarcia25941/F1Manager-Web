1f4651787029dc74564d56a94a3a9ac7fcfc2517
brunogarcia25941 <aluno25941@ipt.pt>
2026-05-29 16:20:54 +0100

    Atualizar pacotes para .NET 9 e aplicar primeira migration no MySQL

---
+++ F1Manager.Web/Migrations/20260529151626_InitialCreate.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace F1Manager.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Campeonatos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Ano = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Campeonatos", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Equipas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    FabricanteMotor = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Pais = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Equipas", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Corridas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    NomeGrandePremio = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Circuito = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    DataHora = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    CampeonatoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Corridas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Corridas_Campeonatos_CampeonatoId",
+                        column: x => x.CampeonatoId,
+                        principalTable: "Campeonatos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Pilotos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    NumeroCarro = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    EquipaId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pilotos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Pilotos_Equipas_EquipaId",
+                        column: x => x.EquipaId,
+                        principalTable: "Equipas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "ResultadosCorridas",
+                columns: table => new
+                {
+                    PilotoId = table.Column<int>(type: "int", nullable: false),
+                    CorridaId = table.Column<int>(type: "int", nullable: false),
+                    PosicaoFinal = table.Column<int>(type: "int", nullable: false),
+                    Pontos = table.Column<int>(type: "int", nullable: false),
+                    TempoVoltaRapida = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ResultadosCorridas", x => new { x.PilotoId, x.CorridaId });
+                    table.ForeignKey(
+                        name: "FK_ResultadosCorridas_Corridas_CorridaId",
+                        column: x => x.CorridaId,
+                        principalTable: "Corridas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ResultadosCorridas_Pilotos_PilotoId",
+                        column: x => x.PilotoId,
+                        principalTable: "Pilotos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Corridas_CampeonatoId",
+                table: "Corridas",
+                column: "CampeonatoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pilotos_EquipaId",
+                table: "Pilotos",
+                column: "EquipaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ResultadosCorridas_CorridaId",
+                table: "ResultadosCorridas",
+                column: "CorridaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ResultadosCorridas");
+
+            migrationBuilder.DropTable(
+                name: "Corridas");
+
+            migrationBuilder.DropTable(
+                name: "Pilotos");
+
+            migrationBuilder.DropTable(
+                name: "Campeonatos");
+
+            migrationBuilder.DropTable(
+                name: "Equipas");
+        }
+    }
+}
