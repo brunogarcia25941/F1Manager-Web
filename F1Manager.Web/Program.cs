97562ce52efb88656672c52853ff536093a23462
João Passarinho <aluno25945@ipt.pt>
2026-06-03 18:22:28 +0100

    Adicionar Swagger UI

--- F1Manager.Web/Program.cs
+++ F1Manager.Web/Program.cs
@@ -26,10 +26,19 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => {
 builder.Services.AddRazorPages();
 builder.Services.AddControllers();
 
+// Configuração do Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+else
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
